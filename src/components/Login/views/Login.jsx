c809d0ec8ba5ab3bed413dadfa6778bf0fbce363
cristhiancao9 <u1802342@unimilitar.edu.co>
2024-09-08 13:30:01 -0500

    Feat: modulo registro y login

--- src/components/Login/views/Login.jsx
+++ src/components/Login/views/Login.jsx
@@ -1,16 +1,29 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import Input from "../../Input/Input";
+import classes from "./Login.module.css";
+const {
+  contenedorRegistro,
+  contenedorForm,
+  titulo,
+  contenedorInput,
+  nombreInput,
+  contenedorSecundarioInput,
+  styleInput,
+  required,
+  btnEnviar,
+} = classes;
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [disabledBtn, setDisabledBtn] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
-
+    setDisabledBtn(true);
     try {
       const response = await fetch("http://localhost:5000/api/auth/login", {
         method: "POST",
@@ -26,10 +39,13 @@ const Login = () => {
         // Guardar el token en localStorage
         localStorage.setItem("token", data.token);
         navigate("/tasks");
+        setDisabledBtn(false);
       } else {
         setError(data.message);
+        setDisabledBtn(false);
       }
     } catch (error) {
+      setDisabledBtn(false);
       setError("Error de red");
     }
   };
@@ -38,20 +54,52 @@ const Login = () => {
     <div>
       <h2>Login</h2>
       {error && <p>{error}</p>}
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Iniciar sesión</button>
+      <form className={contenedorForm} onSubmit={handleSubmit}>
+        <div className={contenedorInput}>
+          <p className={`${nombreInput} && ${required}`}>e-mail</p>
+          <div className={contenedorSecundarioInput}>
+            <Input
+              className={styleInput}
+              value={email}
+              // autoComplete="none"
+              name="email"
+              id="email"
+              minLength={"9"}
+              maxLength="50"
+              type="email"
+              placeholder="e-mail"
+              onChange={(e) => setEmail(e.target.value)}
+              onInvalid={(e) => e.target.setCustomValidity("Add e-mail")}
+              onInput={(e) => e.target.setCustomValidity("")}
+              required
+              title="Enter only valid email"
+            />
+          </div>
+        </div>
+        <div className={contenedorInput}>
+          <p className={`${nombreInput} && ${required}`}>Password</p>
+          <div className={contenedorSecundarioInput}>
+            <Input
+              className={styleInput}
+              value={password}
+              // autoComplete="none"
+              name="password"
+              id="password"
+              minLength={"9"}
+              maxLength="15"
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              onInvalid={(e) => e.target.setCustomValidity("Add password")}
+              onInput={(e) => e.target.setCustomValidity("")}
+              required
+              title="Enter only valid password"
+            />
+          </div>
+        </div>
+        <button disabled={disabledBtn} className={btnEnviar} type="submit">
+          Iniciar sesión
+        </button>
       </form>
     </div>
   );
--- src/components/Register/views/Register.jsx
+++ src/components/Register/views/Register.jsx
@@ -1,18 +1,31 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { notify, notifyError } from "../../../utils/notify";
+import Input from "../../Input/Input";
 import classes from "./Register.module.css";
 
-const { contenedorRegistro, contenedorForm, titulo } = classes;
+const {
+  contenedorRegistro,
+  contenedorForm,
+  titulo,
+  contenedorInput,
+  nombreInput,
+  contenedorSecundarioInput,
+  styleInput,
+  required,
+  btnEnviar,
+} = classes;
 const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [disabledBtn, setDisabledBtn] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("");
-
+    setDisabledBtn(true);
     try {
       const response = await fetch("http://localhost:5000/api/auth/register", {
         method: "POST",
@@ -21,14 +34,21 @@ const Register = () => {
         },
         body: JSON.stringify({ email, password }),
       });
-
+      console.log("response", response);
       if (response.ok) {
-        navigate("/login");
+        const data = await response.json();
+        notify(data?.message);
+        navigate("/public/login");
+        setDisabledBtn(false);
       } else {
         const data = await response.json();
+        console.log("data", data);
+        notifyError(data?.message);
         setError(data.message);
+        setDisabledBtn(false);
       }
     } catch (error) {
+      setDisabledBtn(false);
       setError("Error de red");
     }
   };
@@ -36,21 +56,52 @@ const Register = () => {
   return (
     <div className={contenedorRegistro}>
       <h2 className={titulo}>Sign Up</h2>
-      {error && <p>{error}</p>}
       <form className={contenedorForm} onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Correo electrónico"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Contraseña"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Registrar</button>
+        <div className={contenedorInput}>
+          <p className={`${nombreInput} && ${required}`}>e-mail</p>
+          <div className={contenedorSecundarioInput}>
+            <Input
+              className={styleInput}
+              value={email}
+              // autoComplete="none"
+              name="email"
+              id="email"
+              minLength={"9"}
+              maxLength="50"
+              type="email"
+              placeholder="e-mail"
+              onChange={(e) => setEmail(e.target.value)}
+              onInvalid={(e) => e.target.setCustomValidity("Add e-mail")}
+              onInput={(e) => e.target.setCustomValidity("")}
+              required
+              title="Enter only valid email"
+            />
+          </div>
+        </div>
+        <div className={contenedorInput}>
+          <p className={`${nombreInput} && ${required}`}>Password</p>
+          <div className={contenedorSecundarioInput}>
+            <Input
+              className={styleInput}
+              value={password}
+              // autoComplete="none"
+              name="password"
+              id="password"
+              minLength={"9"}
+              maxLength="15"
+              type="password"
+              placeholder="password"
+              onChange={(e) => setPassword(e.target.value)}
+              onInvalid={(e) => e.target.setCustomValidity("Add password")}
+              onInput={(e) => e.target.setCustomValidity("")}
+              required
+              title="Enter only valid password"
+            />
+          </div>
+        </div>
+        <button disabled={disabledBtn} className={btnEnviar} type="submit">
+          REGISTRAR
+        </button>
       </form>
     </div>
   );
