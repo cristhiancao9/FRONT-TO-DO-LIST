ae06b6af432dea1ec98977a50f784cac2e50ac11
cristhiancao9 <u1802342@unimilitar.edu.co>
2024-09-08 15:23:53 -0500

    feat: ruta privada

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./app/Login/views/Login";
 import Register from "./app/Register/views/Register";
 import TaskList from "./app/TaskList/views/TaskList";
+import PrivateRoute from "./layout/private/Private";
 const PublicLayout = lazy(() => import("./layout/public/Public"));
 function App() {
   return (
     <Routes>
-      <Route path="/tasks" element={<TaskList />} />
+      <Route
+        path="/tasks"
+        element={
+          <PrivateRoute>
+            <TaskList />
+          </PrivateRoute>
+        }
+      />
+      {/* <Route path="/tasks" element={<TaskList />} /> */}
       <Route path="/public" element={<PublicLayout />}>
         <Route path="register" element={<Register />} />
         <Route path="login" element={<Login />} />
