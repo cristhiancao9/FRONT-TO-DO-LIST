f4eb99022f8a941a4cc1eeb769461d0f0458b320
cristhiancao9 <u1802342@unimilitar.edu.co>
2024-09-08 12:10:02 -0500

    Feat: tostContariner para notificaciones

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,20 @@ import { createRoot } from "react-dom/client";
 import React, { Suspense } from "react";
 import App from "./App.jsx";
 import "./index.css";
+import { ToastContainer } from "react-toastify";
 import { BrowserRouter as Router } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <>
+    <ToastContainer
+      position={"top-center"}
+      autoClose={5000}
+      hideProgressBar={false}
+      closeOnClick={true}
+      pauseOnHover={true}
+      draggable={true}
+      progress={undefined}
+    />
     <Suspense fallback={<div>Loading...</div>}>
       <Router>
         <App />
